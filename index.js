c1aef1bd60e4dd0e0d33115c2cd05d283de8bb7b
ziyadanbari <ziyadanbari@gmail.com>
2024-03-15 01:13:28 +0000

    edit url regex

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/api/hello", function (req, res) {
 });
 app.post("/api/shorturl", async function (req, res) {
   const urlRegex =
-    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+    /^https?:\/\/(?:www\.)?[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(?:\/[^\/]*)?$/gi;
   const { url = "" } = req.body;
   const isUrlMatch = urlRegex.test(url);
   if (!isUrlMatch) return res.status(400).json({ error: "invalid url" });
